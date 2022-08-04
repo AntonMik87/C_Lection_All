39f8ceb85e7216c458b2ed7f08a2df70cc4fbc78
Anton <hranitel-liks@mail.ru>
2022-08-04 16:12:29 +0700

    Expmle007 Cycle

---
+++ Example007_Cycle/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+//Concole.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 1, ya = 1,
+    xb = 1, yb = 30,
+    xc = 60, yc = 30;
+
+Concole.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Concole.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Concole.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
