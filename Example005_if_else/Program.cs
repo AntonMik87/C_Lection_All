87feb9a7e48c5f9ad9aa6bcfb6c35627124de442
Anton <hranitel-liks@mail.ru>
2022-08-04 15:11:14 +0700

    Expmle005 if else

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Наташа")
+{
+    Console.WriteLine("Ура, это же Наташа!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
