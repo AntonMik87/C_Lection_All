5a18d039cfabb118df26c3c3352699fb18a46003
«Anton» <hranitel-liks@mail.ru>
2022-08-20 20:51:31 +0700

    Lection3HW

--- Lection_3/Упорядочевание _данных_массива/Program.cs
+++ Lection_3/Упорядочевание _данных_массива/Program.cs
@@ -1,4 +1,4 @@
-﻿// упорядочить массив 6 8 3 2 1 4 5 7
+﻿// упорядочить массив  1, 5, 4, 3, 2, 6, 7, 1, 1
 Console.Clear();
 int[] array = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
 
@@ -14,22 +14,49 @@ void PrintArray(int[] array)
 }
 Console.WriteLine();
 
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length -1; i++)
+//     {
+//         int minPos = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPos]) minPos = j;
+//         }
+
+//         int temporaru = array[i];
+//         array[i] = array[minPos];
+//         array[minPos] = temporaru;
+//     }
+
+// }
+// Вывод от большего к меньшему  1, 5, 4, 3, 2, 6, 7, 1, 1
 void SelectionSort(int[] array)
 {
     for (int i = 0; i < array.Length -1; i++)
     {
-        int minPos = i;
+        int maxPos = i;
         for (int j = i + 1; j < array.Length; j++)
         {
-            if (array[j] < array[minPos]) minPos = j;
+             if (array[j] > array[maxPos]) maxPos = j;
         }
 
-        int temporaru = array[i];
-        array[i] = array[minPos];
-        array[minPos] = temporaru;
+        // int temporaru = array[i];
+        // array[i] = array[maxPos];
+        // array[maxPos] = temporaru;
     }
 
 }
+
+
+// Вывод в обратном порядке
+// void SelectionSort(int[] array)
+// {
+//     for (int j = array.Length - 1; j >= 0; j--)
+//     {
+//         Console.Write($"{array[j]} ");
+//     }
+// }
 
 PrintArray(array);
 Console.WriteLine();
